d235aba14499b32926a0cc44c6cc5e46113d633b
Pawel Lesniak <lesniak.pawel.86@gmail.com>
2019-12-01 21:51:29 +0100

    a bit of a cleanup

--- src/App.js
+++ src/App.js
@@ -1,93 +1,9 @@
 import React, { Component } from 'react';
-import Person from './Person/Person';
 import Assignment from './Assignment/Assignment';
-import UserOutput from './Assignment/UserOutput/UserOutput';
 import './App.css';
 
 class App extends Component {
-	state = {
-		persons: [
-			{
-				name: 'Max',
-				age: '28'
-			},
-			{
-				name: 'Manu',
-				age: '29'
-			},
-			{
-				name: 'Stephanie',
-				age: '26'
-			}
-		],
-		otherState: 'some other value',
-		assignmentState: {
-			persons: [
-				{
-					id: 1,
-					name: 'Nabuchodonozor'
-				},
-				{
-					id: 2,
-					name: 'Emmanuel'
-				},
-				{
-					id: 3,
-					name: 'Solomon'
-				}
-			]
-		}
-	};
-
-	assignmentStateNameChangeHandler = (event) => {
-		const { persons } = this.state.assignmentState;
-		persons[event.target.id - 1].name = event.target.value;
-
-		this.setState({
-			assignmentState: {
-				persons
-			}
-		});
-	};
-
-	switchNameHandler = (newName) => {
-		//console.log('Was clicked!')
-		this.setState({
-			persons: [
-				{
-					name: newName,
-					age: '28'
-				},
-				{
-					name: 'Manu',
-					age: '29'
-				},
-				{
-					name: 'Stephanie',
-					age: '27'
-				}
-			]
-		});
-	};
-
-	nameChangedHandler = (event) => {
-		this.setState({
-			persons: [
-				{
-					name: 'Max',
-					age: '28'
-				},
-				{
-					name: event.target.value,
-					age: '29'
-				},
-				{
-					name: 'Stephanie',
-					age: '27'
-				}
-			]
-		});
-	};
+	state = {};
 
   render() {
   	const style = {
@@ -99,51 +15,12 @@ class App extends Component {
 		};
 
     return (
-      <div className="App">
-				<h1>Hi, I'm a React App!</h1>
-				<p>This is really working!</p>
-
-				<button
-					style={style}
-					onClick={() => this.switchNameHandler('Maximilian!!')}
-				>
-					Switch Name
-				</button>
-
-				<Person
-					name={this.state.persons[0].name}
-					age={this.state.persons[0].age}
-				/>
-
-				<Person
-					name={this.state.persons[1].name}
-					age={this.state.persons[1].age}
-					click={this.switchNameHandler.bind(this, 'Max!')}
-					changed={this.nameChangedHandler}
-				>
-					My Hobbies: Racing
-				</Person>
-
-				<Person
-					name={this.state.persons[2].name}
-					age={this.state.persons[2].age}
-				/>
-
-				<Assignment
-					title='Assignment no. 1'
-				>
-					{this.state.assignmentState.persons.map((elt, index) =>
-						<UserOutput
-							key={index}
-							userId={elt.id}
-							name={elt.name}
-							nameChangeHandler={this.assignmentStateNameChangeHandler}
-						/>
-					)}
-				</Assignment>
-      </div>
+			<Assignment
+				title='Assignment no. 2'
+			>
+				(this is going to be the assignment content)
+			</Assignment>
     );
-	//   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')) ;
   }
 }
 
