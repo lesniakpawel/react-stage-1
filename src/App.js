6e986f0315d42628b52138096969943779a99a8f
Pawel Lesniak <lesniak.pawel.86@gmail.com>
2019-12-04 00:00:39 +0100

    67. Setting class names dynamically

--- src/App.js
+++ src/App.js
@@ -84,10 +84,19 @@ class App extends Component {
   		style.backgroundColor = 'red'
 		}
 
-    return (
+		const classes = [];;
+
+  	if (this.state.persons.length <= 2) {
+  		classes.push('red');
+		}
+		if (this.state.persons <= 1) {
+			classes.push('bold');
+		}
+
+		return (
       <div className="App">
 				<h1>Hi, I'm a React App!</h1>
-				<p>This is really working!</p>
+				<p className={classes.join(' ')}>This is really working!</p>
 
 				<button
 					style={style}
